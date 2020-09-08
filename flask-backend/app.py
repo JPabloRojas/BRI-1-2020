6465f8a5c1d68620f4414ca136007986e687e878
JPabloRojas <juan.rojas.r@usach.cl>
2020-09-08 01:35:31 -0300

    vincular consultas de elastic a flask

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from demo import querys
+from elastic.elastic import *
 
 app = Flask(__name__)
+elastic_class_connection = ESConnection()
+
+@app.route('/query', methods=['POST'])
+def sendQuery():
+    parse_query = request.json['query'].split(' ')
+    response = elastic_class_connection.search_keywords(parse_query)
+    return_response = []
+    for hit in response:
+        return_response.append(hit['info'].to_dict())
+    return jsonify(return_response)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)# tambien se puede indicar el port
